4ec5e514ca65da886a97336f1d13c27c8d8c7041
caue41 <69259714+caue41@users.noreply.github.com>
2020-10-01 11:55:23 -0300

    Project created

---
+++ backend/src/main/java/com/devsuperior/trabalhoFinalCap1caue/TrabalhoFinalCap1CaueApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.trabalhoFinalCap1caue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrabalhoFinalCap1CaueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrabalhoFinalCap1CaueApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/devsuperior/trabalhoFinalCap1caue/TrabalhoFinalCap1CaueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.trabalhoFinalCap1caue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrabalhoFinalCap1CaueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
