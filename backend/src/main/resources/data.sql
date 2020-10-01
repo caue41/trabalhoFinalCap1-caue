0e9ebc8a2bbd643fdfdd11c5b28abb0fe7514bb1
caue41 <69259714+caue41@users.noreply.github.com>
2020-10-01 15:09:33 -0300

    Database seeding

---
+++ backend/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('José Roberto', '111.222.333-45', 3000.00, '13/11/1981', 2);
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Maria José', '444.222.666-76', 3500.00, '14/03/1994', 1);
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Pedro Paulo', '222.555.333-89', 2000.00, '12/11/1991', 3);
